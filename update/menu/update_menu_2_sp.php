c7e12cef7a5e55b35fef87ef2ba14f702dc834f2
ts03511 <ts03511@gmail.com>
2019-11-10 22:32:05 +0900

    iPhone11Proに合わせてサイズ調整

--- update/menu/update_menu_2_sp.php
+++ update/menu/update_menu_2_sp.php
@@ -1,40 +1,50 @@
 <!DOCTYPE html>
 <head>
-	<title>retort-pack</title>
-	<meta charset="UTF-8">
-	<link rel="stylesheet" type="text/css" href="./css/update_menu_2_sp.css">
-	
+    <title>retort-pack</title>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" type="text/css" href="./css/update_menu_2_sp.css">	
 </head>
 
 <!-- List of valiables.
-	$update_sql -> For Update Data SQL.
-	$sql_result -> exec SQL Result.
+    $update_sql -> For Update Data SQL.
+    $sql_result -> exec SQL Result.
 -->
 
-	<body>
-		<?php
-			require '../../common/connect_to_db.php';
-	        
-            $update_sql = "UPDATE beer_menu_tran SET 
-		        brewery=\"{$_POST['brewery']}\",
-		        beername=\"{$_POST['beername']}\",
-		        value=\"{$_POST['value']}\"
-		        WHERE no=\"{$_POST['no']}\";
-	        ";
+<body>
+    <?php
+        require '../../common/connect_to_db.php';
 
-			$db_connect->query($update_sql);
-		?>
+        $update_sql = "
+            UPDATE 
+                beer_menu_tran 
+            SET 
+                brewery=\"{$_POST['brewery']}\",
+                beername=\"{$_POST['beername']}\",
+                value=\"{$_POST['value']}\"
+            WHERE 
+                no=\"{$_POST['no']}\";
+        ";
 
-		<div id="master">
-			<h3>この内容で更新します。</h3>
-                <?php
-                   print "<p>{$_POST['brewery']}</p>";
-                   print "<p>{$_POST['beername']}</p>";
-                   print "<p>{$_POST['value']}</p>";
-                ?>
+        $db_connect->query($update_sql);
+    ?>
 
-			<input type="button" value="決定" onClick="location.href='./update_menu_3_sp.php'"><br><br> 
-			<input type="button" value="キャンセル" onClick="location.href='./update_menu_1_sp.php'">
-		</div>
-	</body>
+    <div id="master">
+        <div id="update_title">
+            この内容で更新します。
+        </div>
+
+        <div id="update_summary">
+            <?php
+                print "<p>{$_POST['brewery']}</p>";
+                print "<p>{$_POST['beername']}</p>";
+                print "<p>{$_POST['value']}</p>";
+            ?>
+        </div>
+
+        <input type="button" value="決定" onClick="location.href='./update_menu_3_sp.php'"><br><br> 
+        <input type="button" value="キャンセル" onClick="location.href='./update_menu_1_sp.php'">
+    </div>
+
+</body>
+
 </html>
